7f91d6a6d50520b35a4c054e737e271ca69d3268
Ginny (Khue) Dang <58489398+ginny100@users.noreply.github.com>
2021-08-13 09:44:07 -0500

    Update nodejs.groovy

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,8 +1,8 @@
 job('NodeJS example') {
     scm {
-        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        git('git://github.com/ginny100/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('Ginny Dang')
+            node / gitConfigEmail('ginnydang0728@gmail.com')
         }
     }
     triggers {
